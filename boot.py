425b410e22bb7bac76f88b2748dce0b0aa92db1d
Jonas Scharpf <jonas@brainelectronics.de>
2023-03-24 14:38:04 +0100

    add port specific variable definition for Winbond init

--- boot.py
+++ boot.py
@@ -6,18 +6,56 @@
 
 from machine import SPI, Pin
 import os
-from winbond import W25QFlash
+from winbond import W25QFlash, version
 
-# Pin   HSPI (id=1)   VSPI (id=2)
-# -------------------------------
-# sck   14            18
-# mosi  13            23
-# miso  12            19
-# cs    x, here 5     x, here 5
-flash = W25QFlash(spi=SPI(2),
-                  cs=Pin(5),
-                  baud=2000000,
-                  software_reset=True)
+os_info = os.uname()
+print('MicroPython infos: {}'.format(os_info))
+print('Used micropthon-winbond version: {}'.format(version.__version__))
+
+try:
+    if 'pyboard' in os_info:
+        # NOT YET TESTED !
+        # https://docs.micropython.org/en/latest/library/pyb.SPI.html#pyb.SPI
+        CS_PIN = Pin(1)
+        spi = SPI(1)
+    elif 'esp8266' in os_info:
+        # NOT YET TESTED !
+        # https://docs.micropython.org/en/latest/esp8266/quickref.html#hardware-spi-bus
+        # SPI(0) is used for FlashROM and not available to users
+        # highest possible baudrate is 40 MHz for ESP-12
+        CS_PIN = Pin(1)
+        spi = SPI(1)
+    elif 'esp32' in os_info:
+        # https://docs.micropython.org/en/latest/esp32/quickref.html#hardware-spi-bus
+        # Pin   HSPI (id=1)   VSPI (id=2)
+        # -------------------------------
+        # sck   14            18
+        # mosi  13            23
+        # miso  12            19
+        # cs    x, here 5     x, here 5
+        CS_PIN = Pin(5)
+        spi = SPI(2)
+    elif 'rp2' in os_info:
+        # https://docs.micropython.org/en/latest/rp2/quickref.html#hardware-spi-bus
+        # Pin   id=0   id=1
+        # -------------------------------
+        # sck   6            10
+        # mosi  7            11
+        # miso  4            8
+        # cs    x, here 5    x, here 5
+        CS_PIN = Pin(5)
+        spi = SPI(0)
+    else:
+        raise Exception(
+            'Unknown device, no default values for CS_PIN and spi defined: {}'.
+            format(os_info)
+        )
+except AttributeError:
+    pass
+except Exception as e:
+    raise e
+
+flash = W25QFlash(spi=spi, cs=CS_PIN, baud=2000000, software_reset=True)
 
 flash_mount_point = '/external'
 
