36a88075cdb755a7eb5d3550d31ffdebf42454ea
Rik Starmans <hstarmans@hexastorm.com>
2024-05-30 10:14:40 +0200

    updated changelog and version

--- winbond/version.py
+++ winbond/version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-__version_info__ = ("0", "5", "2")
-__version__ = '.'.join(__version_info__)
+__version_info__ = ("0", "5", "3")
+__version__ = ".".join(__version_info__)
